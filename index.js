dd01bfac50359a13b49570969d914fd67480a231
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2021-10-28 16:54:25 +0200

    Index

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require("firebase/firestore");
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use(cors({
   allowedHeaders:["*"],
